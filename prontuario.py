7748b1e866d506ca469484ca3c32b46b97431ee8
ThalesCaruso <carusothales@gmail.com>
2022-06-03 22:23:07 -0300

    criacao do prontuario.py sem funções ainda

---
+++ prontuario.py
@@ -0,0 +1,11 @@
+class Prontuario: 
+
+    def __init__(self, historico, exames, anamnese, avalicaoinicial, prognostico, medicacao):
+        self.historico = historico
+        self.exames = exames
+        self.anamnese = anamnese
+        self.avaliacaoinicial = avalicaoinicial
+        self.prognostico = prognostico
+        self.medicacao = medicacao
+
+        
