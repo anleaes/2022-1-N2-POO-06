4cad4efa39fb95c1616f768ce7313e560e89b939
gabgcs <gabrielgcslol@gmail.com>
2022-06-07 10:19:14 -0300

    Criando a classe Historico

--- historico.py
+++ historico.py
@@ -1,4 +1,8 @@
 class Historico:
 
-    def __init__ ():
+    def __init__ (self, doenca_previa, doenca_tratada, historico_familiar, grau_parentesco):
+        self._doenca_previa = doenca_previa
+        self._doenca_tratada = doenca_tratada
+        self._historico_familiar = historico_familiar
+        self._grau_parentesco = grau_parentesco
         
