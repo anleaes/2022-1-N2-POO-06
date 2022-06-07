0b3591bc7766860884dd343d2fba196bba40b82b
gabgcs <gabrielgcslol@gmail.com>
2022-06-07 10:42:39 -0300

    Construtor classe Prognostico

--- prognostico.py
+++ prognostico.py
@@ -1,3 +1,8 @@
 class Prognostico:
 
-    def __init__ ():
+    def __init__ (self, medicacao, exame, especificadores, encaminhamentos):
+        self._medicacao = medicacao
+        self._exame = exame
+        self._especificadores = especificadores
+        self._encaminhamentos = encaminhamentos
+        
