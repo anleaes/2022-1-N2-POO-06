7edaa5fb5a012b2dd083eb5fb206cb6e87defa30
ThalesCaruso <carusothales@gmail.com>
2022-06-07 20:08:58 -0300

    classe medicacao

---
+++ medicacao.py
@@ -0,0 +1,7 @@
+class Medicacao:
+
+    def __init__ (self, classe, tempo, nome, motivo):
+        self.classe = classe
+        self.tempo = tempo
+        self.nome = nome
+        self.motivo = motivo
