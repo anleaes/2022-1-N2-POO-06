70e365ace4bb20e8f4cce4e470cc11d8a9917739
ThalesCaruso <carusothales@gmail.com>
2022-06-07 22:06:45 -0300

    alteracoes de privado e publico

--- exame.py
+++ exame.py
@@ -1,8 +1,8 @@
 class Exame:
 
     def __init__ (self, data, tipo, resultado, avaliacao):
-        self._data = data
-        self._tipo = tipo
-        self._resultado = resultado
-        self._avaliacao = avaliacao
+        self.data = data
+        self.tipo = tipo
+        self.resultado = resultado
+        self.avaliacao = avaliacao
         
